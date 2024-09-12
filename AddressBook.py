b5434d391a8c3f8b77c3a0426dd10d5c9563fddc
vijay1 <vijaykumarynh7@gmail.com>
2024-09-12 11:25:20 +0530

    [Vijay] add: Python Program to display the welcome message.

---
+++ AddressBook.py
@@ -0,0 +1,27 @@
+"""
+@Author:Vijay Kumar M N
+@Date: 2024-09-11
+@Last Modified by: Vijay Kumar M N
+@Last Modified: 2024-09-11
+@Title : python program for the AddressBook.
+"""
+
+
+   
+def main():
+    
+    """
+    Description:
+        This function is used to display the welcome message.
+
+    Parameters:
+        None
+    
+    Returns:
+        None
+    """
+    print("-----------------------Welcome to Address Book--------------------")
+
+  
+if __name__=="__main__":
+    main()
