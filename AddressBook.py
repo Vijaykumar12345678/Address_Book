7719e789fb3b943eb46071eb7438f5204ac02764
vijay1 <vijaykumarynh7@gmail.com>
2024-09-12 13:32:22 +0530

    [Vijay] add: Python program on create address book class and add contacts.

---
+++ AddressBook.py
@@ -0,0 +1,138 @@
+"""
+@Author:Vijay Kumar M N
+@Date: 2024-09-12
+@Last Modified by: Vijay Kumar M N
+@Last Modified: 2024-09-12
+@Title : python program for the AddressBook to add the contacts.
+"""
+from MyLogging import logger_init
+class contact:
+    def __init__(self,First_Name,Last_Name,Address,City,State,Zip_Code,Phone_Number,Email):
+        self.First_Name=First_Name
+        self.Last_Name=Last_Name
+        self.Address=Address
+        self.City=City
+        self.State=State
+        self.Zip_Code=Zip_Code
+        self.Phone_Number=Phone_Number
+        self.Email=Email
+
+
+    def display_Contacts(self):
+        """
+        Description:
+            This function is used to print the contacts
+            
+        Parameters:
+            None
+
+        Returns:
+            str: all the contacts
+            """
+        return (f"Name: {self.First_Name} {self.Last_Name}\n"
+                f"Address: {self.Address}, {self.City}, {self.State}, {self.Zip_Code}\n"
+                f"Phone: {self.Phone_Number}\n"
+                f"Email: {self.Email}\n")
+
+class AddressBook:
+    def __init__(self):
+        self.contacts={}
+    
+    def add_contact(self,contact):
+        
+        """
+        Description:
+            This method is used to add a contact to the address book.
+             If the contact already exists, it will print a message.
+        
+        Parameters:
+            contact (Contact): The contact object to be added.
+        
+        Returns:
+            None
+        
+        """
+       
+        key=f"{contact.First_Name} {contact.Last_Name}"
+        if key not in self.contacts:
+            self.contacts[key]=contact
+            print("Contact Added Successfully")
+            logger_init("UC_2").info(f"Contact Added  Successfully:")
+
+        else:
+            print("Contact already exists")
+            logger_init("UC_2").info(f"Contact already Exists:")
+    
+    def display_all_contacts(self):
+
+        """
+        Description:
+            This function is used to display all the contacts.
+        
+        Parameters:
+            None
+        
+        Returns:
+            result: str 
+        """
+        
+        if not self.contacts:
+            result="\nNo contacts found in the Address Book.\n"
+        else:
+            result="\n All Contacts:\n"
+            for contact in self.contacts.values():
+                result+=contact.display_Contacts()
+            return result
+
+def main():
+    
+    """
+    Description:
+        This function is used to display the welcome message.
+
+    Parameters:
+        None
+    
+    Returns:
+        None
+    """
+    print("-----------------------Welcome to Address Book--------------------")
+
+    address_book=AddressBook()
+    
+    while True:
+        print("\n------ADDRESS BOOK MENU-------\n")
+        print('\n 1. Add new Contact:')
+        print("\n 2. Display contacts")
+
+        choice=int(input("Enter the choice:"))
+        if choice==1:    
+            First_Name=input("Enter the First Name:")
+            Last_Name=input("Enter the Last Name:")
+            Address=input("Enter the Address:")
+            City=input("Enter the City:")
+            State=input("Enter the State:")
+            Zip_Code=input("Enter the Zip Code:")
+            Phone_Number=int(input("Enter the Phone Number:"))
+            Email=input("Enter the Email:")
+    
+            contacts=contact(
+                First_Name=First_Name,
+                Last_Name=Last_Name,
+                Address=Address,
+                City=City,
+                State=State,
+                Zip_Code=Zip_Code,
+                Phone_Number=Phone_Number,
+                Email=Email
+            )
+            address_book.add_contact(contacts)
+            
+        elif choice==2:
+            result=address_book.display_all_contacts()
+            print(result)
+        else:
+            break
+
+if __name__=="__main__":
+    main()
